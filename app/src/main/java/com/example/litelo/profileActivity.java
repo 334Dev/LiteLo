2906df8228f0b099e52ed0a627bb5e2143b0b2a1
Anant Shukla <anantshukla1402@gmail.com>
2020-10-05 18:42:55 +0530

    Profile UI

--- app/src/main/java/com/example/litelo/profileActivity.java
+++ app/src/main/java/com/example/litelo/profileActivity.java
@@ -1,17 +1,16 @@
 package com.example.litelo;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -20,7 +19,6 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.StorageTask;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 import com.theartofdev.edmodo.cropper.CropImage;
@@ -31,7 +29,8 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class profileActivity extends AppCompatActivity {
 
             private CircleImageView profileImageView;
-            private TextView profile_name,profile_Regno,profile_Grp,logoutBtn;
+            private TextView profile_name,profile_Regno,profile_Grp;
+            private Button logoutBtn;
 
             private FirebaseAuth mAuth;
 
