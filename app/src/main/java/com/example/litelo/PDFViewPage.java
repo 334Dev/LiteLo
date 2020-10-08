2ec573fe82bd60d4f75a9bee04f368930fdc2640
Anant Shukla <anantshukla1402@gmail.com>
2020-10-08 13:24:21 +0530

    share Pdf Added

--- app/src/main/java/com/example/litelo/PDFViewPage.java
+++ app/src/main/java/com/example/litelo/PDFViewPage.java
@@ -1,20 +1,25 @@
 package com.example.litelo;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.os.Environment;
-
 import com.github.barteksc.pdfviewer.PDFView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.File;
 
 public class PDFViewPage extends AppCompatActivity {
     PDFView pdfView;
     File file;
+    private FloatingActionButton shareButton;
 
 
     @Override
@@ -24,6 +29,17 @@ public class PDFViewPage extends AppCompatActivity {
         pdfView=findViewById(R.id.pdfView);
         file=new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/Lite.pdf");
         pdfView.fromFile(file).load();
+        shareButton=findViewById(R.id.shareButton);
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                Uri pdfUri = Uri.parse(Environment.getExternalStorageDirectory().getAbsolutePath()+"/Lite.pdf");
+                sharingIntent.setType("*/*");
+                sharingIntent.putExtra(Intent.EXTRA_STREAM, pdfUri);
+                startActivity(Intent.createChooser(sharingIntent, "Share using"));
+            }
+        });
 
 
         ActivityCompat.requestPermissions(this,new String[]{
