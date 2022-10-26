3c9848c1b96149d0ed3811479c187100c178c302
mitrukahitesh <mitrukahitesh@gmail.com>
2022-10-26 21:56:49 +0530

    fixed setting page admin and contact us button

--- app/src/main/java/com/dev334/litelo/UI/settings/SettingsFragment.java
+++ app/src/main/java/com/dev334/litelo/UI/settings/SettingsFragment.java
@@ -5,19 +5,16 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
-import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.dev334.litelo.AdminActivity;
 import com.dev334.litelo.ChangePassword;
