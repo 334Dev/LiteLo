d9bfa57a9a0c54f9a856ad1b111ebf3bbba1a1bf
hackeramitkumar <amit9116260192@gmail.com>
2022-10-29 13:17:23 +0530

    added notification fragment

--- app/src/main/java/com/dev334/litelo/UI/notification/NotificationAdapter.java
+++ app/src/main/java/com/dev334/litelo/UI/notification/NotificationAdapter.java
@@ -1,64 +1,70 @@
 package com.dev334.litelo.UI.notification;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.dev334.litelo.EventActivity;
+import com.dev334.litelo.EventAdapter;
 import com.dev334.litelo.R;
+import com.dev334.litelo.model.EventModel;
+import com.dev334.litelo.utility.Constants;
 
 import java.util.List;
 
-public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.mViewholder> {
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.CustomVH> {
 
-    private List<String> resourceModels;
+    private List<NotificationModel> notifications;
+    Context context;
 
-    private onNoteListener mOnNoteListener;
-    public NotificationAdapter(List<String> resourceModels, onNoteListener onNoteListener){
-        this.resourceModels=resourceModels;
-        this.mOnNoteListener=onNoteListener;
+    public NotificationAdapter(List<NotificationModel> notifications, Context context) {
+        this.notifications = notifications;
+        this.context = context;
     }
 
     @NonNull
     @Override
-    public mViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.notification_list_item, parent, false);
-        return new NotificationAdapter.mViewholder(view,mOnNoteListener);
+    public NotificationAdapter.CustomVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new NotificationAdapter.CustomVH(View.inflate(parent.getContext(), R.layout.notification_list_item, null));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull mViewholder holder, int position) {
-//        holder.setSubjectName(resourceModels.get(position));
+    public void onBindViewHolder(@NonNull NotificationAdapter.CustomVH holder, int position) {
+        holder.setView(notifications.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return resourceModels.size();
-    }
-    public interface onNoteListener{
-        void onNoteClick(int position);
+        return notifications.size();
     }
 
-    public class mViewholder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public class CustomVH extends RecyclerView.ViewHolder {
+        private TextView name, desc,time;
 
-        View view;
-        TextView Subject;
-        onNoteListener onNotelistener;
-        public mViewholder(@NonNull View itemView, onNoteListener onNotelistener) {
+        public CustomVH(@NonNull View itemView) {
             super(itemView);
-            view=itemView;
-            itemView.setOnClickListener(this);
-            this.onNotelistener=onNotelistener;
+            name = itemView.findViewById(R.id.name);
+            desc = itemView.findViewById(R.id.notification_desc_tv);
+            time = itemView.findViewById(R.id.date_time);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
         }
 
+        public void setView(NotificationModel notificationModel) {
+            name.setText(notificationModel.getName());
+            desc.setText(notificationModel.getDesc());
 
-        @Override
-        public void onClick(View view) {
-            onNotelistener.onNoteClick(getAdapterPosition());
         }
     }
-
 }
--- app/src/main/java/com/dev334/litelo/UI/notification/NotificationFragment.java
+++ app/src/main/java/com/dev334/litelo/UI/notification/NotificationFragment.java
@@ -1,84 +1,43 @@
 package com.dev334.litelo.UI.notification;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.dev334.litelo.R;
-import com.dev334.litelo.ClubModel;
-import com.dev334.litelo.UpcomingAdapter;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
 import java.util.List;
 
-public class NotificationFragment extends Fragment implements UpcomingAdapter.onNoteListener {
+public class NotificationFragment extends Fragment {
 
     private View view;
-    private RecyclerView clubRecycler;
+    private RecyclerView notificationRecyclerView;
     private FirebaseFirestore firestore;
-    private List<ClubModel> clubModels;
-    private UpcomingAdapter clubAdapter;
+    private NotificationAdapter notificationAdapter;
+    private List<NotificationModel> notifications;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view= inflater.inflate(R.layout.fragment_notification, container, false);
-        clubRecycler=view.findViewById(R.id.notifactionRec);
+        fetchNotifications();
+        notificationRecyclerView=view.findViewById(R.id.notifaction_recycler);
+        notificationRecyclerView.setAdapter(notificationAdapter);
+        notificationRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        notificationRecyclerView.setHasFixedSize(true);
 
-        clubModels=new ArrayList<>();
-        clubAdapter= new UpcomingAdapter(clubModels, this);
-        clubRecycler.setAdapter(clubAdapter);
-        clubRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
-        clubRecycler.setHasFixedSize(true);
+//        firestore=FirebaseFirestore.getInstance();
 
-        firestore=FirebaseFirestore.getInstance();
-
-        firestore.collection("ClubClasses").document("CC").collection("Classes")
-                .whereEqualTo("visibility", "show")
-                .whereGreaterThan("close",System.currentTimeMillis())
-                .orderBy("close", Query.Direction.ASCENDING)
-                .get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-            @Override
-            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                if(queryDocumentSnapshots.isEmpty()){
-                    Log.i("ClubClasses", "onSuccess: Empty");
-                }else {
-                    List<DocumentSnapshot> snapshots=queryDocumentSnapshots.getDocuments();
-                    for(DocumentSnapshot snapshot:snapshots){
-                        clubModels.add(snapshot.toObject(ClubModel.class));
-                    }
-                    clubAdapter.notifyDataSetChanged();
-                }
-            }
-        });
         return view;
     }
 
-    @Override
-    public void onNoteClick(int position) {
-        Log.i("recyclerViewOnClick", "recyclerviewOnClick: Clicked");
-        String link=clubModels.get(position).getLink();
-        if(link=="N/A"){
-            Toast.makeText(getContext(),"No link provided by admin", Toast.LENGTH_SHORT).show();
-        }else{
-            Intent i = new Intent(Intent.ACTION_VIEW);
-            i.setData(Uri.parse(link));
-            startActivity(i);
-        }
+    private void fetchNotifications() {
+
+
     }
+
 }
