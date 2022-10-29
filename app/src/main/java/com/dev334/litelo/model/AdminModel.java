d8225250ba95023f9c9a9384bc1e30b3f46e1ed9
hackeramitkumar <amit9116260192@gmail.com>
2022-10-29 10:58:10 +0530

    Added admin_model

---
+++ app/src/main/java/com/dev334/litelo/model/AdminModel.java
@@ -0,0 +1,50 @@
+package com.dev334.litelo.model;
+
+public class AdminModel {
+    private String event, eventId, dept,deptId;
+
+
+    public AdminModel(){
+
+    }
+
+    public AdminModel(String event, String eventId, String dept, String deptId) {
+        this.event = event;
+        this.eventId = eventId;
+        this.dept = dept;
+        this.deptId = deptId;
+    }
+
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public String getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(String deptId) {
+        this.deptId = deptId;
+    }
+}
